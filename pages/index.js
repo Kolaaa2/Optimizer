36153c43d614ac2f5f6a1f45a09eeaec9300f88b
Kolaa_ <karelzavdielzilby@gmail.com>
2026-03-31 17:24:21 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1393,9 +1393,5 @@ export default function Home() {
               IHDR Skip · Web Workers · OGG Safe · Alpha Cleanup · Single-Color · Pow2 · Deep JSON · JSON Sort · .lang · .bbmodel · Shader Minify · SHA-1 · Pack Analyzer · Badge Gen
             </div>
            </footer>
-
-        </>)}
-
-    </>
   );
 }
